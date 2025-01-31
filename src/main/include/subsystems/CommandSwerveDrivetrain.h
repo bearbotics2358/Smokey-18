1cde47e9681fd865fd3614bfa5e6b2dd3f0b14bb
Brock Atchison <aldryd@gmail.com>
2025-01-30 22:26:02 -0600

    Integrate the path planner library
    Originally from work Charlie and Vishy did. The commits got a little
    wonky so this Pull Request is an effort to pull in only the necessary
    changes for path planner.

--- src/main/include/subsystems/CommandSwerveDrivetrain.h
+++ src/main/include/subsystems/CommandSwerveDrivetrain.h
@@ -30,6 +30,9 @@ class CommandSwerveDrivetrain : public frc2::SubsystemBase, public TunerSwerveDr
     /* Keep track if we've ever applied the operator perspective before or not */
     bool m_hasAppliedOperatorPerspective = false;
 
+    /** Swerve request to apply during robot-centric path following */
+    swerve::requests::ApplyRobotSpeeds m_pathApplyRobotSpeeds;
+
     /* Swerve requests to apply during SysId characterization */
     swerve::requests::SysIdSwerveTranslation m_translationCharacterization;
     swerve::requests::SysIdSwerveSteerGains m_steerCharacterization;
@@ -125,6 +128,7 @@ public:
         if (utils::IsSimulation()) {
             StartSimThread();
         }
+        ConfigureAutoBuilder();
     }
 
     /**
@@ -151,6 +155,7 @@ public:
         if (utils::IsSimulation()) {
             StartSimThread();
         }
+        ConfigureAutoBuilder();
     }
 
     /**
@@ -184,6 +189,7 @@ public:
         if (utils::IsSimulation()) {
             StartSimThread();
         }
+        ConfigureAutoBuilder();
     }
 
     /**
@@ -249,6 +255,7 @@ public:
     }
 
 private:
+    void ConfigureAutoBuilder();
     void StartSimThread();
 };
 
