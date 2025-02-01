97f76adee1627b5d46b97ff10f3e315330bda650
Charlie <malohc1025@lz95.net>
2025-02-01 12:47:27 -0600

    Final Save - This should work

--- src/main/cpp/subsystems/CameraSubsystem.cpp
+++ src/main/cpp/subsystems/CameraSubsystem.cpp
@@ -45,35 +45,6 @@ void CameraSubsystem::Periodic() {
         frc::SmartDashboard::PutNumber("Y To Best Tag", y);
         frc::SmartDashboard::PutNumber("Rotation To Best Tag", rot);
         frc::SmartDashboard::PutNumber("Best Tag ID", bestTarget.GetFiducialId());
-
-        double tagAngle;
-        switch (bestTarget.GetFiducialId()){
-            case 7:
-            case 10:
-            case 18:
-            case 21:
-                tagAngle = M_PI / 2;
-                break;
-            case 17:
-            case 20:
-            case 11:
-            case 8:
-                tagAngle = M_PI / 3;
-                break;
-            case 19:
-            case 22:
-            case 9:
-            case 6:
-                tagAngle = 2 * M_PI / 3;
-                break;
-        }
-
-        //turn rot degrees
-        //move parallel to tag until yaw = 0
-        //raise elevator
-        //move perpendicular to tag until ready to score
-
-
     } else {
         frc::SmartDashboard::PutNumber("Yaw To Best Tag", 0);
         frc::SmartDashboard::PutNumber("X To Best Tag", 0);
