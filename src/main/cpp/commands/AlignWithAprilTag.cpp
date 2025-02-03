ee00932413973647ab1dbcff4391b51a5fbd4ef3
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-02-03 16:53:04 -0600

    removed AlignWithAprilTag files.

--- src/main/cpp/commands/AlignWithAprilTag.cpp
+++
@@ -1,6 +0,0 @@
-#include <commands/AlignWithAprilTag.h>
-
-AlignWithAprilTag::AlignWithAprilTag(subsystems::CommandSwerveDrivetrain* drivetrain, int aprilTagId) {
-    m_aprilTagId = aprilTagId;
-    AddRequirements(drivetrain);
-}
