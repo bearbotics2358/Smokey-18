62fc2cbed25be8a89b539ff6ebaed42e7a75310b
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-02-06 16:22:12 -0600

    made ThroughBoreEncoder more reusable

--- src/main/cpp/subsystems/ThroughBoreEncoder.cpp
+++ src/main/cpp/subsystems/ThroughBoreEncoder.cpp
@@ -1,12 +1,23 @@
 #include <subsystems/ThroughBoreEncoder.h>
 
-ThroughBoreEncoder::ThroughBoreEncoder() {};
+ThroughBoreEncoder::ThroughBoreEncoder(int dioPortA, int dioPortB): 
+m_throughBoreEncoder{dioPortA, dioPortB, false, frc::Encoder::EncodingType::k4X} 
+{
+    // The number of pulses per revolution.
+    m_throughBoreEncoder.SetDistancePerPulse(1.0 / 2048.0);
+    // Resets the distance to zero.
+    m_throughBoreEncoder.Reset();
+};
 
 // The two functions below are examples
 double ThroughBoreEncoder::GetDistance() {
-    return m_throughBorEncoder.GetDistance();
+    return m_throughBoreEncoder.GetDistance();
 };
 
 double ThroughBoreEncoder::GetDirection() {
-    return m_throughBorEncoder.GetDirection();
+    return m_throughBoreEncoder.GetDirection();
 };
+
+void ThroughBoreEncoder::ResetEncoder() {
+    m_throughBoreEncoder.Reset();
+}
--- src/main/include/subsystems/ThroughBoreEncoder.h
+++ src/main/include/subsystems/ThroughBoreEncoder.h
@@ -5,10 +5,10 @@
 
 class ThroughBoreEncoder : public frc2::SubsystemBase {
     public:
-        ThroughBoreEncoder();
+        ThroughBoreEncoder(int dioPortA, int dioPortB);
         double GetDistance();
         double GetDirection();
+        void ResetEncoder();
     private:
-        // TODO: {0, 1} sets up the DIO ports on 0 and 1. Change accordingly.
-        frc::Encoder m_throughBorEncoder{0, 1, false, frc::Encoder::EncodingType::k4X};
+        frc::Encoder m_throughBoreEncoder;
 };
