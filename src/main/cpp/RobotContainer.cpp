c1f742d4141291b21cb1d9ef345df4a6821f39ef
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-02-11 09:18:17 -0600

    added command bindings to buttons.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -30,12 +30,17 @@ void RobotContainer::ConfigureBindings() {
     joystick.LeftBumper().OnTrue(drivetrain.RunOnce([this] { drivetrain.SeedFieldCentric(); }));
     drivetrain.RegisterTelemetry([this](auto const &state) { logger.Telemeterize(state); });
 
+    // Button Map
+    m_gamepad.Button(12).OnTrue(m_led.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L1));
+    m_gamepad.Button(11).OnTrue(m_led.SetLEDState(ArduinoConstants::RIO_MESSAGES::ALGAE_HELD));
+    m_gamepad.Button(10).OnTrue(m_led.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L2));
     m_gamepad.Button(9).OnTrue(m_led.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L3));
+    m_gamepad.Button(8).OnTrue(m_led.SetLEDState(ArduinoConstants::RIO_MESSAGES::IDK));
 
     // Example for running parallel commands with a single button press
-    m_gamepad.Button(10).OnTrue(frc2::cmd::Parallel(
-        m_led.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L2),
-        frc2::cmd::RunOnce([this] { frc::SmartDashboard::PutString("Elevator", "L2"); })));
+    // m_gamepad.Button(10).OnTrue(frc2::cmd::Parallel(
+    //     m_led.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L2),
+    //     frc2::cmd::RunOnce([this] { frc::SmartDashboard::PutString("Elevator", "L2"); })));
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
