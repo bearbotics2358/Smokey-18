24af225e3fced4e01c69582f41060bdf6c66865a
hdjwis <adarshyraju@gmail.com>
2025-02-15 16:44:56 -0600

    Add parsing for the Coral Feather CAN data

--- src/main/include/io/FeatherCanDecoder.h
+++ src/main/include/io/FeatherCanDecoder.h
@@ -2,6 +2,9 @@
 
 #include "subsystems/ICoralIntakeDataProvider.h"
 
+#include <frc/CAN.h>
+
+
 class FeatherCanDecoder: public ICoralIntakeDataProvider {
 public:
     FeatherCanDecoder();
@@ -11,8 +14,13 @@ public:
     // **** ICoralIntakeDataProvider interface functions **** //
     float GetCoralIntakeAngleDegrees() override;
     bool IsCoralCollected() override;
+    const int kCoralCanID = 1; // @todo this is temporary, change to actual ID
+    const int kCoralAPIId = 4;
 
 private:
     float m_coralIntakeAngleDegrees;
     bool m_coralCollected;
+    frc::CAN m_coralCAN;
+
+    void UnpackCoralCANData();
 };
