3fc0446ecae465c038d4a6940e7c80e891c23c3d
Brock Atchison <aldryd@gmail.com>
2025-02-22 21:43:30 -0600

    Questionable changes added on Saturday at Week 0 event
    This was able to run the elevator up and down using the SetControl
    with the DutyCycleOut parameter in an open loop; however, we were
    not able to get the motors to work together when trying to use
    SetControl with a specific position. The two motors were fighting
    each other instead of having motor 2 follow motor 1.

--- src/main/include/subsystems/ElevatorSubsystem.h
+++ src/main/include/subsystems/ElevatorSubsystem.h
@@ -10,7 +10,7 @@
 
 constexpr int kElevatorMotor1Id = 36;
 constexpr int kElevatorMotor2Id = 37;
-constexpr int kLimitSwitchId = 1;
+constexpr int kLimitSwitchId = 0;
 
 // @todo Assign these to real values when we know the distances
 constexpr units::inch_t kElevatorStowPosition = 0_in;
@@ -18,6 +18,8 @@ constexpr units::inch_t kElevatorL1Position = 0_in;
 constexpr units::inch_t kElevatorL2Position = 0_in;
 constexpr units::inch_t kElevatorL3Position = 0_in;
 constexpr units::inch_t kElevatorL4Position = 0_in;
+
+constexpr float kSlowElevator = 0.5;
 
 class ElevatorSubsystem : public frc2::SubsystemBase {
     public:
@@ -28,8 +30,25 @@ class ElevatorSubsystem : public frc2::SubsystemBase {
 
         frc2::CommandPtr SetPositionCommand(units::inch_t position);
 
+        frc2::CommandPtr Stop();
+
+        // Test function to slowly raise the elevator
+        frc2::CommandPtr Raise();
+
+        // Test function to slowly lower the elevator
+        frc2::CommandPtr Lower();
+
+        units::inch_t CurrentHeight();
+        units::turn_t HeightToTurns(units::inch_t height);
+
+        const units::inch_t WHEEL_RADIUS = 1.5_in;
+        // 9 to 1
+        const double GEAR_RATIO = 9.0;
+
     private:
         ctre::phoenix6::hardware::TalonFX m_elevatorMotor1;
         ctre::phoenix6::hardware::TalonFX m_elevatorMotor2;
         frc::DigitalInput m_elevatorLimitSwitch;
+
+        ctre::phoenix6::controls::PositionVoltage m_positionVoltage = ctre::phoenix6::controls::PositionVoltage{0_tr}.WithSlot(0);
 };
