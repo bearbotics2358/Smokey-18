1c21f02494389abfec264181c9619775d3469520
Charlie <malohc1025@lz95.net>
2025-03-01 13:11:37 -0600

    Blueprint for the climber class Constants need to be tuned

--- src/main/cpp/subsystems/Climber.cpp
+++ src/main/cpp/subsystems/Climber.cpp
@@ -1,21 +1,68 @@
 #include "subsystems/Climber.h"
 #include <frc/smartdashboard/SmartDashboard.h>
 
-Climber::Climber()
+Climber::Climber():
+    m_climberMotor(kClimberMotor1Id)
 {
+    ctre::phoenix6::configs::MotorOutputConfigs motorConfigs;
+    motorConfigs.WithNeutralMode(ctre::phoenix6::signals::NeutralModeValue::Brake)
+        .WithInverted(true)
+        // Limit the forward and reverse duty cycle while getting the climber working.
+        .WithPeakForwardDutyCycle(kSlowClimber)
+        .WithPeakReverseDutyCycle(kSlowClimber);
+
+    m_climberMotor.GetConfigurator().Apply(motorConfigs);
+    
+    m_climberMotor.SetPosition(0_tr);
+    /*
+     * This method blocks the current robot loop until the signal is retrieved or the timeout is activated.
+     * The CTRE docs state that this API can ensure that set operations are completed before continuing control flow.
+     * This method reports an error to the DriverStation.
+     * The link: https://v6.docs.ctr-electronics.com/en/stable/docs/api-reference/api-usage/status-signals.html
+     */ 
+    m_climberMotor.GetPosition().WaitForUpdate(20_ms);
 }
 
 void Climber::Periodic() {
+    frc::SmartDashboard::PutNumber("Raw Climber Turns", CurrentTurns().value());
+
+    frc::SmartDashboard::PutNumber("Climber Setpoint", m_setpointTurns.value());
+
+    SetMotorVoltage();
+}
+
+units::turn_t Climber::CurrentTurns() {
+    return units::turn_t(m_climberMotor.GetPosition().GetValueAsDouble());
 }
 
 frc2::CommandPtr Climber::Climb() {
-    return frc2::cmd::Run([this] {
-        // @todo Implement the ability to climb
+    return frc2::cmd::RunOnce([this] {
+        m_setpointTurns = kClimberEndTurns;
     });
 }
 
 frc2::CommandPtr Climber::CancelClimb() {
-    return frc2::cmd::Run([this] {
-        // @todo Implement the ability to cancel a climb safely
+    return frc2::cmd::RunOnce([this] {
+        m_setpointTurns = kClimberStartTurns;
     });
+}
+
+frc2::CommandPtr Climber::Stow() {
+    return frc2::cmd::RunOnce([this] {
+        m_setpointTurns = kClimberStowTunrs;
+    });
+}
+
+void Climber::SetMotorVoltage() {
+    double value = m_elevatorPID.Calculate(CurrentTurns(), m_setpointTurns);
+    frc::SmartDashboard::PutNumber("Climber PID", value);
+
+    units::volt_t goalVolts = units::volt_t(value);
+    frc::SmartDashboard::PutNumber("Climber PID in Volts", goalVolts.value());
+
+    m_climberMotor.SetVoltage(goalVolts);
+}
+
+frc2::CommandPtr Climber::StopClimbMotor() {
+    return frc2::cmd::RunOnce([this] {m_climberMotor.StopMotor();});
 }
--- src/main/include/subsystems/Climber.h
+++ src/main/include/subsystems/Climber.h
@@ -2,6 +2,24 @@
 
 #include <frc2/command/SubsystemBase.h>
 #include <frc2/command/Commands.h>
+#include <ctre/phoenix6/TalonFX.hpp>
+#include <units/length.h>
+#include <units/velocity.h>
+#include <units/acceleration.h>
+#include <units/angle.h>
+#include <frc/controller/ProfiledPIDController.h>
+#include <frc/trajectory/TrapezoidProfile.h>
+
+// @todo All constants here are TEMPORARY - change them as needed
+constexpr int kClimberMotor1Id = 50;
+
+constexpr float kSlowClimber = 0.5;
+
+constexpr units::turn_t kClimberStartTurns = 20.0_tr;
+constexpr units::turn_t kClimberEndTurns = 10.0_tr;
+constexpr units::turn_t kClimberStowTunrs = 0.0_tr;
+
+const double kClimberGearRatio = 1.0;
 
 class Climber : public frc2::SubsystemBase {
 public:
@@ -10,4 +28,27 @@ public:
 
     frc2::CommandPtr Climb();
     frc2::CommandPtr CancelClimb();
+    frc2::CommandPtr Stow();
+    void SetMotorVoltage();
+    units::turn_t CurrentTurns();
+
+    frc2::CommandPtr StopClimbMotor();
+
+private:
+    ctre::phoenix6::hardware::TalonFX m_climberMotor;
+
+        static constexpr units::meters_per_second_t kMaxVelocity = 0.25_mps;
+        static constexpr units::meters_per_second_squared_t kMaxAcceleration = 0.75_mps_sq;
+        static constexpr double kP = 1.0;
+        static constexpr double kI = 1.0;
+        static constexpr double kD = 0.0;
+
+        frc::TrapezoidProfile<units::meters>::Constraints m_constraints {
+            kMaxVelocity, kMaxAcceleration};
+
+        frc::ProfiledPIDController<units::meters> m_elevatorPID{
+            kP, kI, kD, m_constraints
+        };
+
+        units::turn_t m_setpointTurns = 0_tr;
 };
