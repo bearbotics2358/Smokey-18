cae7503a3ce2ce11d9a98f69f6d86c9369862175
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-03-08 14:43:18 -0600

    added endgame switch implementation

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -47,26 +47,41 @@ void RobotContainer::ConfigureBindings() {
     //         frc2::cmd::RunOnce([this] {m_speedMultiplier = 1.0;})
     //     );
 
-    m_gamepad.Button(12).OnChange(frc2::cmd::RunOnce([this] {
+     m_gamepad.Button(7).OnTrue(frc2::cmd::Parallel(
+        frc2::cmd::RunOnce([this] {
+            m_drivetrain.SetSwervesNeutralValue(ctre::phoenix6::signals::NeutralModeValue::Coast);
+        }),
+        m_climberSubsystem.GoToAngle(kClimberEndAngle)
+    ));
+
+    m_gamepad.Button(12).OnTrue(frc2::cmd::RunOnce([this] {
         m_elevatorSubsystem.PrepareElevator(kElevatorL4Position);
-        m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::IDK); 
-        }));
-    m_gamepad.Button(11).OnChange(frc2::cmd::RunOnce([this] {
+        //m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::IDK); 
+    }));
+
+    m_gamepad.Button(11).OnTrue(frc2::cmd::RunOnce([this] {
         m_elevatorSubsystem.PrepareElevator(kElevatorL3Position);
-        m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L3);
-        }));
-    m_gamepad.Button(10).OnChange(frc2::cmd::RunOnce([this] { 
+        //m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L3);
+    }));
+
+    m_gamepad.Button(10).OnTrue(frc2::cmd::RunOnce([this] { 
         m_elevatorSubsystem.PrepareElevator(kElevatorL2Position); 
-        m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L2);
-        }));
-    m_gamepad.Button(9).OnChange(frc2::cmd::RunOnce([this] {
-         m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::ALGAE_HELD);
-        }));
-    m_gamepad.Button(8).OnChange(frc2::cmd::RunOnce([this] { 
+        //m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L2);
+    }));
+
+    m_gamepad.Button(9).OnTrue(frc2::cmd::RunOnce([this] {
+        //m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::ALGAE_HELD);
+    }));
+
+    m_gamepad.Button(8).OnTrue(frc2::cmd::RunOnce([this] { 
         m_elevatorSubsystem.PrepareElevator(kElevatorL1Position); 
-        m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L1);
-        }));
-    m_gamepad.Button(17).OnChange(frc2::cmd::RunOnce([this] { m_elevatorSubsystem.PrepareElevator(kElevatorStowPosition); }));    //button below 8 on universal driverstation for stow position
+        //m_LED.SetLEDState(ArduinoConstants::RIO_MESSAGES::ELEVATOR_L1);
+    }));
+
+    //button below 8 on universal Driver Station for stow position
+    m_gamepad.Button(17).OnTrue(frc2::cmd::RunOnce([this] { 
+            m_elevatorSubsystem.PrepareElevator(kElevatorStowPosition); 
+    }));
 
     (m_joystick.X() && m_joystick.Y()).WhileTrue(m_cameraSubsystem.RunOnce([this] {frc::SmartDashboard::PutNumber("YDistance", m_cameraSubsystem.getYDistance());} ));
 
