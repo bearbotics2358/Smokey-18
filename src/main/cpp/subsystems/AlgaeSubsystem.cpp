53039fcf97b6424d2824c8f4b92030a982692c95
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-03-15 15:07:14 -0500

    added timeout to the algae Intake Command

--- src/main/cpp/subsystems/AlgaeSubsystem.cpp
+++ src/main/cpp/subsystems/AlgaeSubsystem.cpp
@@ -38,7 +38,7 @@ frc2::CommandPtr AlgaeSubsystem::Intake() {
         [this] {
             return m_algaeDataProvider->IsAlgaeCollected();
         }
-    );
+    ).WithTimeout(3_s);
 }
 
 frc2::CommandPtr AlgaeSubsystem::Dispense() {
