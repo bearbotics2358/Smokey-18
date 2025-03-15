cf70e846d4d087fd958a9ff3e292ad03770fb049
Charlie <malohc1025@lz95.net>
2025-03-15 09:38:09 -0500

    sets KG to 0_V when the setpoint is 0 so that the elevator just rests on the bottom and triggers the limit switch

--- src/main/cpp/subsystems/ElevatorSubsystem.cpp
+++ src/main/cpp/subsystems/ElevatorSubsystem.cpp
@@ -83,6 +83,9 @@ void ElevatorSubsystem::SetMotorVoltage() {
     if (current_difference >= TOLERANCE) {
         m_elevatorMotor1.SetVoltage(goalVolts);
         m_elevatorMotor2.SetVoltage(goalVolts);
+    } else if (m_setpointHeight == 0.0_in) {
+        m_elevatorMotor1.SetVoltage(0_V);
+        m_elevatorMotor2.SetVoltage(0_V);
     } else {
         m_elevatorMotor1.SetVoltage(kG);
         m_elevatorMotor2.SetVoltage(kG);
