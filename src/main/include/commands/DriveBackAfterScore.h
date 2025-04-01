2bd425e7a4b2a22df1f35f557a44e6e445a3ff89
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-04-01 10:42:36 -0500

    added drivebackafterscore.h comments

--- src/main/include/commands/DriveBackAfterScore.h
+++ src/main/include/commands/DriveBackAfterScore.h
@@ -1,3 +1,7 @@
+/**
+ * @file DriveBackAfterScore.h
+ */
+
 #pragma once
 
 #include <frc2/command/Command.h>
@@ -7,11 +11,22 @@
 
 #include <frc/controller/PIDController.h>
 
+/**
+ * @brief This class moves the robot back after scoring a coral.
+ */
 class DriveBackAfterScore
     : public frc2::CommandHelper<frc2::Command, DriveBackAfterScore> {
 public:
+    /**
+     * @brief The default backward distance is 6 inches.
+     */
     static constexpr units::inch_t kDefaultDistance = 6_in;
 
+    /**
+     * @brief This constructor takes in the drivetrain and a distance. The default backward distance is 6 inches.
+     * @param[in] drivetrain This command takes in the drivetrain.
+     * @param[in] distance This is an optional parameter; the default distance is 6 inches.
+     */
     explicit DriveBackAfterScore(
         subsystems::CommandSwerveDrivetrain* drivetrain,
         units::inch_t distance = kDefaultDistance
@@ -19,8 +34,17 @@ public:
     void Initialize() override;
     void Execute() override;
     bool IsFinished() override;
+    /**
+     * @brief After finishing the command, the robot's speed is zero to ensure the robot stops moving back.
+     */
     void End(bool interrupted) override;
 
+    /**
+     * @brief This helper function is intended to calculate the current distance traveled.
+     * @param[in] first The first `frc::Pose2d`.
+     * @param[in] second The second `frc::Pose2d`.
+     * @return The Euclidean distance between the two poses, which is calculated using the Pythagorean theorem.
+     */
     units::inch_t GetDistance(frc::Pose2d first, frc::Pose2d second);
 
 private:
