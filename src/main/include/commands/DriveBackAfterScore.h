7f54bf7df6f40a76c79aa4248dd8cffae2624794
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-04-01 11:09:35 -0500

    added driveforwardtoscore

--- src/main/include/commands/DriveBackAfterScore.h
+++ src/main/include/commands/DriveBackAfterScore.h
@@ -22,6 +22,11 @@ public:
      */
     static constexpr units::inch_t kDefaultDistance = 6_in;
 
+    /**
+     * @brief This constructor takes in the drivetrain and a distance. The default backward distance is 6 inches.
+     * @param[in] drivetrain This command takes in the drivetrain.
+     * @param[in] distance This is an optional parameter; the default distance is 6 inches.
+     */
     explicit DriveBackAfterScore(
         subsystems::CommandSwerveDrivetrain* drivetrain,
         units::inch_t distance = kDefaultDistance
@@ -29,8 +34,17 @@ public:
     void Initialize() override;
     void Execute() override;
     bool IsFinished() override;
+    /**
+     * @brief After finishing the command, the robot's speed is zero to ensure the robot stops moving back.
+     */
     void End(bool interrupted) override;
 
+    /**
+     * @brief This helper function is intended to calculate the current distance traveled.
+     * @param[in] first The first `frc::Pose2d`.
+     * @param[in] second The second `frc::Pose2d`.
+     * @return The Euclidean distance between the two poses, which is calculated using the Pythagorean theorem.
+     */
     units::inch_t GetDistance(frc::Pose2d first, frc::Pose2d second);
 
 private:
