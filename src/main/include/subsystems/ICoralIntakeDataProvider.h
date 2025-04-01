54d6b4f400043781243803a8464875c1ff0e88ed
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-04-01 15:24:42 -0500

    all the interface docs have been "completed"

--- src/main/include/subsystems/ICoralIntakeDataProvider.h
+++ src/main/include/subsystems/ICoralIntakeDataProvider.h
@@ -1,15 +1,31 @@
+/**
+ * @file IClimberDataProvider.h
+ */
+
 #pragma once
 
+/**
+ * @brief Contains all the functions necessary for the coral arm.
+ */
 class ICoralIntakeDataProvider {
 public:
-    // Provide the angle of the coral intake/delivery mechanism in degrees
+    /**
+     * @brief Provide the angle of the coral intake/delivery mechanism in degrees.
+     */ 
     virtual float GetCoralIntakeAngleDegrees() = 0;
 
-    // Provide the raw angle of the coral mechanism. Does not include any offsets.
+    /** 
+     * @brief Provide the raw angle of the coral mechanism. Does not include any offsets.
+     */ 
     virtual float GetCoralIntakeRawAngleDegrees() = 0;
 
+    /** 
+     * @brief Determines whether the **absolute** coral angle data is valid.
+     */ 
     virtual bool IsCoralAngleValid() = 0;
 
-    // Return a simple boolean to indicate whether a piece of coral has been collected
+    /** 
+     * @brief Return a simple boolean to indicate whether a piece of coral has been collected
+     */ 
     virtual bool IsCoralCollected() = 0;
 };
