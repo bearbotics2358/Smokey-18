8303e3ecf69096cd914fbad71c882a6ba50daaf3
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-04-01 19:28:11 -0500

    robot

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -2,6 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/**
+ * @file Robot.h
+ */
 #pragma once
 
 #include <optional>
@@ -13,6 +16,12 @@
 
 #include <io/FeatherCanDecoder.h>
 
+/**
+ * @brief Runs the robot code.
+ * @attention All of the subsystem declaration and trigger bindings should happen in
+ * Robot Container.
+ * @see RobotContainer.h
+ */
 class Robot : public frc::TimedRobot {
  public:
   Robot();
@@ -30,6 +39,10 @@ class Robot : public frc::TimedRobot {
   void TestPeriodic() override;
   void TestExit() override;
 
+  /**
+   * @brief Represents the device that decodes CAN messages to get various sensor values.
+   * @note This variable will be passed into various subsystems as a pointer.
+   */
   FeatherCanDecoder m_featherCanDecoder;
  private:
   frc2::Command *m_autonomousCommand;
