13c402419d871b8da65712c25a5e97b33e4cf066
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-04-01 19:02:33 -0500

    finished camera subsystem

--- src/main/include/subsystems/CameraSubsystem.h
+++ src/main/include/subsystems/CameraSubsystem.h
@@ -23,14 +23,55 @@ class CameraSubsystem : public frc2::SubsystemBase {
  public:
   CameraSubsystem(subsystems::CommandSwerveDrivetrain* drivetrain);
 
+  /**
+   * @brief This function parses camera data.
+   * @details This updates local variables, such as the current best AprilTag and
+   * the transformation to it. This function __must__ be called periodically to
+   * ensure accurate data.
+   */
   void updateData();
+
+  /**
+   * @brief Returns whether AprilTag(s) exist.
+   */
   bool visibleTargets();
+
+  /**
+   * @brief Returns the Y **robot oriented** distance from the camera to the best
+   * AprilTag.
+   * @return The Y distance in meters.
+   */
   units::meter_t getStrafeTransformation();
+
+  /**
+   * @brief Returns the X **robot oriented** distance from the camera to the best
+   * AprilTag.
+   * @return The X distance in meters.
+   */
   units::meter_t getForwardTransformation();
+
+  /**
+   * @brief Returns the straight line distance from the camera to the AprilTag.
+   * AprilTag.
+   * @return The distance in meters.
+   */
   double getDistance();
+
+  /**
+   * @brief Returns the Z rotation needed to orient the camera to the AprilTag.
+   * @return The distance in degrees.
+   */
   units::degree_t getZRotation();
+
+  /**
+   * @brief Returns the rotation needed to orient the camera to the AprilTag.
+   * @return The rotation is a `frc::Rotation2d`.
+   */
   frc::Rotation2d GetRotation2d();
 
+  /**
+   * @brief Returns the ID of the best AprilTag, __if__ it exists.
+   */
   std::optional<int> GetTargetTagId();
 
   void Periodic() override;
