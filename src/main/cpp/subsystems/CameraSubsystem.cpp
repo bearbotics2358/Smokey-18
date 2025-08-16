49e34a61b92c126ea40d197a95e1a1edf322db46
Bearbotics Laptop Sparkmax Stickers <bearbotics2358@gmail.com>
2025-08-16 15:18:49 -0500

    Progress 8/16/25

--- src/main/cpp/subsystems/CameraSubsystem.cpp
+++ src/main/cpp/subsystems/CameraSubsystem.cpp
@@ -4,8 +4,8 @@
 CameraSubsystem::CameraSubsystem(subsystems::CommandSwerveDrivetrain* drivetrain) {
     m_drivetrain = drivetrain;
     robotToCam = 
-        frc::Transform3d(frc::Translation3d(12_in, 34_in, 0_in),
-            frc::Rotation3d(-45_deg, 230.5_rad, 135.3_rad));
+        frc::Transform3d(frc::Translation3d(-15_in, 0_in, 0_in),
+            frc::Rotation3d(-20_deg, 0_rad, 0_rad));
     m_poseEstimator = std::make_unique<photon::PhotonPoseEstimator>(aprilTagFieldLayout, photon::PoseStrategy::MULTI_TAG_PNP_ON_COPROCESSOR, robotToCam);
 }
 
@@ -21,19 +21,21 @@ void CameraSubsystem::updateData() {
         lTransformation = frc::Transform3d(units::meter_t(lBotPose[9]),
                                             units::meter_t(lTargetPose.at(0)),
                                             units::meter_t(lTargetPose.at(2)),
-                                            frc::Rotation3d(units::angle::radian_t(lTargetPose.at(5)),
-                                                            units::angle::radian_t(lTargetPose.at(4)),
-                                                            units::angle::radian_t(lTargetPose.at(3))));
+                                            frc::Rotation3d(units::angle::degree_t(lTargetPose.at(5)),
+                                                            units::angle::degree_t(lTargetPose.at(4)),
+                                                            units::angle::degree_t(lTargetPose.at(3))));
 
         LimelightHelpers::PoseEstimate estimatedPose = LimelightHelpers::getBotPoseEstimate_wpiBlue(kLimelight4);
         m_drivetrain->AddVisionMeasurement(estimatedPose.pose, ctre::phoenix6::utils::FPGAToCurrentTime(estimatedPose.timestampSeconds));
 
         frc::SmartDashboard::PutNumber("lStrafe Distance", units::inch_t(lGetStrafeTransformation()).value());
         frc::SmartDashboard::PutNumber("lForward Distance", units::inch_t(lGetForwardTransformation()).value());
-        frc::SmartDashboard::PutNumber("lForward Distance Mega", m_drivetrain->GetPose().X().value() - 14.2);
+        //frc::SmartDashboard::PutNumber("lForward Distance Mega", m_drivetrain->GetPose().X().value() - units::meter_t(14.2_in).value());
+        frc::SmartDashboard::PutNumber("MegaX", m_drivetrain->GetPose().X().value());
+        frc::SmartDashboard::PutNumber("MegaY", m_drivetrain->GetPose().Y().value());
         frc::SmartDashboard::PutNumber("LNEW DIST", ((sqrt(pow(lBotPose[9], 2)) - pow(0.305, 2)) + 0.43));
-        frc::SmartDashboard::PutNumber("IDONTLIKETHIS", (sqrt((pow(getDistance(), 2) + pow(0.50, 2)) - (2 * getDistance() * 0.50 * cosf(90 + getRotZ())))));
-        frc::SmartDashboard::PutNumber("IDONTLIKETHIS2", (sqrt((pow(lBotPose[9], 2) + pow(0.50, 2)) - (2 * lBotPose[9] * 0.50 * cosf(90 + lBotPose[6])))));
+        frc::SmartDashboard::PutNumber("IDONTLIKETHIS", (sqrt((pow(getDistance(), 2) + pow(0.50, 2)) - (2 * getDistance() * 0.50 * cosh(90 + getRotZ())))));
+        frc::SmartDashboard::PutNumber("IDONTLIKETHIS2", (sqrt((pow(lBotPose[9], 2) + pow(0.50, 2)) - (2 * lBotPose[9] * 0.50 * cosh(90 + lBotPose[6])))));
     }
 
     frc::SmartDashboard::PutBoolean("lHas Targets", lVisibleTargets());
@@ -42,12 +44,17 @@ void CameraSubsystem::updateData() {
     if (result.HasTargets()) {
         bestTarget = result.GetBestTarget();
         transformation = bestTarget.GetBestCameraToTarget();
+    
+        frc::Transform3d robotToTarget = transformation + robotToCam;
+
+        frc::SmartDashboard::PutNumber("PRE OFFSET STRAFE", units::inch_t(transformation.Y()).value());
+        frc::SmartDashboard::PutNumber("PRE OFFSET FORAWRD", units::inch_t(transformation.X()).value());
+        frc::SmartDashboard::PutNumber("Offset Strafe Distance", units::inch_t(robotToTarget.Y()).value());
+        frc::SmartDashboard::PutNumber("Offset Forward Distance", units::inch_t(robotToTarget.X()).value());
+
         frc::Translation3d robotTrans = robotToCam.Translation() + transformation.Translation();
         frc::Rotation3d robotRot = robotToCam.Rotation() + transformation.Rotation();
         frc::Pose3d robotPose = frc::Pose3d(robotTrans, robotRot);
-
-        // frc::SmartDashboard::PutNumber("NEW Strafe Distance", );
-        // frc::SmartDashboard::PutNumber("NEW Forward Distance", );
 
         std::optional<photon::EstimatedRobotPose> estimatedPose = m_poseEstimator->Update(result);
         if (estimatedPose){
@@ -61,6 +68,7 @@ void CameraSubsystem::updateData() {
         frc::SmartDashboard::PutNumber("Rotation", (transformation.Rotation().Z().value() / fabs((transformation.Rotation().Z()).value())) * (180 - fabs(units::degree_t(transformation.Rotation().Z()).value())));
         frc::SmartDashboard::PutNumber("DISTANCE ANGLE", units::degree_t(bestTarget.GetYaw()).value());
         frc::SmartDashboard::PutNumber("Strafe Distance", units::inch_t(getStrafeTransformation()).value());
+        //frc::SmartDashboard::PutNumber("Offset Strafe Distance", robotTrans[1]);  
         frc::SmartDashboard::PutNumber("Forward Distance", units::inch_t(getForwardTransformation()).value());
         frc::SmartDashboard::PutNumber("Dist", units::inch_t(getDistance()).value());
         frc::SmartDashboard::PutNumber("NEW DIST", ((sqrt(pow(getDistance(), 2)) - pow(0.305, 2)) + 0.43));
@@ -80,8 +88,9 @@ std::optional<int> CameraSubsystem::GetTargetTagId() {
 
 double CameraSubsystem::getRotZ() {
     if (visibleTargets()) {
-        return (transformation.Rotation().Z().value() / fabs((transformation.Rotation().Z()).value())) * 
-        (180 - fabs(units::degree_t(transformation.Rotation().Z()).value()));
+        // return (transformation.Rotation().Z().value() / fabs((transformation.Rotation().Z()).value())) * 
+        // (180 - fabs(units::degree_t(transformation.Rotation().Z()).value()));
+        return transformation.Rotation().Z().value();
     } else {
         return 0;
     }
